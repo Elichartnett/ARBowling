98bcd78f12d6e77da73e535b6ddbb384db6df0de
elichartnett <elichartnett@icloud.com>
2023-06-11 19:41:37 -0400

    Initial Commit

---
+++ ARBowling/ARBowlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARBowlingApp.swift
+//  ARBowling
+//
+//  Created by Eli Hartnett on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ARBowlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
