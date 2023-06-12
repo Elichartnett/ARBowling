09a1e92f2a94591597a119e503432a4bdb0a8efb
elichartnett <elichartnett@icloud.com>
2023-06-12 19:47:16 -0400

    Part 2

--- ARBowling/ARGameView.swift
+++ ARBowling/ARGameView.swift
@@ -11,7 +11,11 @@ import RealityKit
 class ARGameView: ARView {
     
     func startApplyingForce(direction: ForceDirection) {
-        
+//        if let bowlingScene = scene.anchors.first(where: { newScene in
+//            newScene.name == MyScene.BowlingScene().name
+//        }) as? MyScene.BowlingScene {
+//            bowlingScene.notifications.forceBehaviorTrigger.post()
+//        }
     }
     
     func stopApplyingForce() {
